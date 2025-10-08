3c3fe159a655ce94e28a53f976a7b714b567b523
Sofia Garcia <sartefia@gmail.com>
2025-10-07 22:33:25 -0300

    itemListContainer catalogo

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,33 @@
 import "./App.css";
-import {ItemListContainer} from "./components/ItemListContainer";
-import {NavBar} from "./components/NavBar";
-import {Container} from "./components/Container";
+import { ItemListContainer } from "./components/ItemListContainer";
+import { NavBar } from "./components/NavBar";
+import { Container } from "./components/Container";
 import { useEffect, useState } from "react";
 
-
 function App() {
-  const [elementos, setElementos]=useState([]);
-  useEffect(()=>{
+  const [elementos, setElementos] = useState([]);
+  useEffect(() => {
     fetch("../public/elementos.json")
-    .then((response)=>response.json())
-    .then((data)=>{
-      setElementos(data)
-      })
-  },[])
-  return(
+      .then((response) => response.json())
+      .then((data) => {
+        setElementos(data);
+        console.log(data);
+      });
+  }, []);
+  return (
     <>
-  <div>
-    <NavBar inicio="Inicio" nosotros="Nosotros"  productos="Productos"/>
-  </div>
-  <ItemListContainer mensaje="Agregá algún producto">
-    <ul>
-      {elementos.map((elemento)=>(      
-        <li key={elemento.id}>{elemento.nombre}</li>))}
-    </ul>
-  </ItemListContainer>
-
-  <div>
-    <Container mensaje="Bienvenidx a nuestro catálogo!"></Container>
-  </div>
+      <div>
+        <NavBar inicio="Inicio" nosotros="Nosotros" productos="Productos" />
+      </div>
+      <main>
+        <Container mensaje="Bienvenidx a nuestro catálogo!"></Container>
+        <ItemListContainer
+          mensaje="Agregá algún producto"
+          elementos={elementos}
+        ></ItemListContainer>
+      </main>
     </>
-  )
+  );
 }
 
 export default App;
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,10 +1,20 @@
-export const ItemListContainer = (props, children) => {
+export const ItemListContainer = (props) => {
     
     return(
         <>
         <div>
             <h3>{props.mensaje}</h3>
-            {children}
+                <ul className="card">
+                    {props.elementos.map((elemento)=>(      
+                    <li key={elemento.id} className="li">
+                        <img src={elemento.img} alt={elemento.nombre} className="img-card"/>
+                        <h4>{elemento.nombre}</h4>
+                        <span>${elemento.costo}</span>
+                        <small>producto número: {elemento.id}</small>
+                        <br />
+                        <button className="li-button">Ver más</button>
+                    </li>))}
+                </ul>
         </div>
         </>
     )
